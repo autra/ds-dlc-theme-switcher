91ff924e970d0bb15d547115f401de6e9a3423d5
Augustin Trancart <augustin.trancart@gmail.com>
2016-10-22 11:13:47 +0200

    First modification: restore RoG soundtrack

---
+++ modinfo.lua
@@ -0,0 +1,19 @@
+name = "RoG Soundtrack restorer"
+
+description = "Restore RoG SoundTrack in main menu."
+
+author = "fisikantix"
+
+version = "0.1"
+
+api_version = 6
+
+forumthread ="https://github.com/autra/ds-rog-soundtrack-restorer"
+
+-- Compatible only with SW. Must have the sound and theme files to change them!
+dont_starve_compatible = false
+reign_of_giants_compatible = false
+shipwrecked_compatible = true
+dst_compatible = false
+
+restart_required = true
---
+++ modmain.lua
@@ -0,0 +1,1 @@
+RemapSoundEvent("dontstarve_DLC002/music/music_FE", "dontstarve/music/music_FE")
